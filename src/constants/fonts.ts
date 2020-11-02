4cccc919e9a7f6578f749dd2c8d74ef17f1ebf50
franciskav <franciska.vigh@gmail.com>
2020-11-02 13:11:10 +0100

    constants

---
+++ src/constants/fonts.ts
@@ -0,0 +1,14 @@
+export const Fonts = {
+  Lato_regular: 'Lato_400Regular',
+  Lato_regular_italic: 'Lato_400Regular_Italic',
+  Lato_bold: 'Lato_700Bold',
+  Lato_bold_italic: 'Lato_700Bold_Italic',
+  Lato_black: 'Lato_900Black',
+  Lato_black_italic: 'Lato_900Black_Italic'
+};
+
+export const FontSizes = {
+  small: 14,
+  normal: 18,
+  large: 25
+};
