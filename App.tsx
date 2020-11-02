da8b7359dc14607afcbe078ff3a6ea9d9458320a
franciskav <franciska.vigh@gmail.com>
2020-11-02 13:10:38 +0100

    login + tab navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,163 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  UIManager,
+  View,
+  Image
+} from 'react-native';
+import { configureStore } from './config/storeconfig';
+import { Screens } from './src/constants/screens';
+import LoginScreen from './src/screens/loginSreen';
+import { Provider } from 'react-redux';
+import SignupScreen from './src/screens/signupScreen';
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_700Bold,
+  Lato_900Black,
+  Lato_400Regular_Italic,
+  Lato_700Bold_Italic,
+  Lato_900Black_Italic
+} from '@expo-google-fonts/lato';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Fonts, FontSizes } from './src/constants/fonts';
+import { Colors } from './src/constants/colors';
+import HomeScreen from './src/screens/homeScreen';
+import TimetableScreen from './src/screens/timetableScreen';
+import AbsenceScreen from './src/screens/absenceScreen';
+import ApplyScreen from './src/screens/applyScreen';
+import ProfilScreen from './src/screens/profilScreen';
+import { Icons } from './src/constants/icons';
 
-export default function App() {
+const RootStack = createStackNavigator();
+const LoginStack = createStackNavigator();
+const TabStack = createBottomTabNavigator();
+
+function RootStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootStack.Navigator mode={'modal'} screenOptions={{ headerShown: false }}>
+      <RootStack.Screen name={'LoginStack'} component={LoginStackScreen} />
+    </RootStack.Navigator>
   );
 }
 
+function LoginStackScreen() {
+  return (
+    <LoginStack.Navigator screenOptions={{ headerShown: false }}>
+      <LoginStack.Screen name={Screens.Login} component={LoginScreen} />
+      <LoginStack.Screen name={Screens.SignUp} component={SignupScreen} />
+      <LoginStack.Screen name={Screens.Home} component={TabStackScreen} />
+    </LoginStack.Navigator>
+  );
+}
+
+function TabStackScreen() {
+  return (
+    <TabStack.Navigator
+      //initialRouteName={Screens.Home}
+      tabBarOptions={{
+        labelStyle: styles.tabText,
+        activeTintColor: Colors.yellow,
+        inactiveTintColor: Colors.lightGrey,
+        style: styles.tabBackground
+      }}
+    >
+      <TabStack.Screen
+        name={Screens.Home}
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Kezdőlap',
+          tabBarIcon: ({ color }) => (
+            <Image source={Icons.home_icon} style={{ tintColor: color }} />
+          )
+        }}
+      />
+      <TabStack.Screen
+        name={Screens.Timetable}
+        component={TimetableScreen}
+        options={{
+          tabBarLabel: 'Órarend',
+          tabBarIcon: ({ color }) => (
+            <Image source={Icons.timetable_icon} style={{ tintColor: color }} />
+          )
+        }}
+      />
+      <TabStack.Screen
+        name={Screens.Apply}
+        component={ApplyScreen}
+        options={{
+          tabBarLabel: 'Jelentkezés',
+          tabBarIcon: ({ color }) => (
+            <Image source={Icons.apply_icon} style={{ tintColor: color }} />
+          )
+        }}
+      />
+      <TabStack.Screen
+        name={Screens.Absence}
+        component={AbsenceScreen}
+        options={{
+          tabBarLabel: 'Hiányzás',
+          tabBarIcon: ({ color }) => (
+            <Image source={Icons.absence_icon} style={{ tintColor: color }} />
+          )
+        }}
+      />
+      <TabStack.Screen
+        name={Screens.Profil}
+        component={ProfilScreen}
+        options={{
+          tabBarLabel: 'Profil',
+          tabBarIcon: ({ color }) => (
+            <Image source={Icons.profil_icon} style={{ tintColor: color }} />
+          )
+        }}
+      />
+    </TabStack.Navigator>
+  );
+}
+
+const App = () => {
+  const store = configureStore();
+
+  let [fonstLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_400Regular_Italic,
+    Lato_700Bold,
+    Lato_700Bold_Italic,
+    Lato_900Black,
+    Lato_900Black_Italic
+  });
+
+  useEffect(() => {
+    if (Platform.OS === 'android')
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+  }, []);
+
+  if (!fonstLoaded) {
+    return <View />;
+  }
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootStackScreen />
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tabText: {
+    fontFamily: Fonts.Lato_regular,
+    fontSize: FontSizes.small
   },
+  tabBackground: {
+    backgroundColor: Colors.darkGrey
+  }
 });
+
+export default App;
