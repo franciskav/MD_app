c91e9008fc71d44eadffdf61ff00272148466b9b
franciskav <franciska.vigh@gmail.com>
2020-11-16 12:14:50 +0100

    fonts

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -10,7 +10,8 @@ export const Fonts = {
 export const FontSizes = {
   extraSmall: 14,
   small: 15,
-  normal: 20,
-  medium: 22,
-  large: 25
+  normal: 18,
+  medium: 20,
+  large: 22,
+  extraLarge: 25
 };
