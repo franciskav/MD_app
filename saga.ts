ef1b2f1234d9be11c99bc8e2f6d02f7a4b406907
franciskav <franciska.vigh@gmail.com>
2020-11-29 13:51:12 +0100

    store, saga

--- saga.ts
+++ saga.ts
@@ -1,5 +1,9 @@
-import {all} from 'redux-saga/effects'
+import { all } from 'redux-saga/effects';
+import { loginSaga } from './src/store/login/login.saga';
+import { logoutSaga } from './src/store/logout/logout.saga';
+import { newsSaga } from './src/store/news/news.saga';
+import { signupSaga } from './src/store/signup/signup.saga';
 
 export function* rootSaga() {
-  yield all([])
+  yield all([loginSaga(), signupSaga(), logoutSaga(), newsSaga()]);
 }
--- store.ts
+++ store.ts
@@ -1,28 +1,47 @@
-import {AnyAction, combineReducers, Reducer} from 'redux'
-export interface IAppStore {}
+import { AnyAction, combineReducers, Reducer } from 'redux';
+import { loginReducer } from './src/store/login/login.reducer';
+import { LoginStore } from './src/store/login/login.store';
+import { logoutReducer } from './src/store/logout/logout.reducer';
+import { LogoutStore } from './src/store/logout/logout.store';
+import { newsReducer } from './src/store/news/news.reducer';
+import { NewsStore } from './src/store/news/news.store';
+import { signupReducer } from './src/store/signup/signup.reducer';
+import { SignupStore } from './src/store/signup/signup.store';
 
-export interface IApplicationState {
-  app: IAppStore
+export interface IAppStore {
+  login: LoginStore;
+  signup: SignupStore;
+  logout: LogoutStore;
+  news: NewsStore;
 }
 
-export const RESET_EVERYTHING = 'RESET_EVERYTHING'
+export interface IApplicationState {
+  app: IAppStore;
+}
+
+export const RESET_EVERYTHING = 'RESET_EVERYTHING';
 
 export interface ResetAction {
-  type: typeof RESET_EVERYTHING
+  type: typeof RESET_EVERYTHING;
 }
 
 export const reset = (): ResetAction => ({
-  type: RESET_EVERYTHING,
-})
+  type: RESET_EVERYTHING
+});
 
-export const appReducer = combineReducers<IAppStore>({})
+export const appReducer = combineReducers<IAppStore>({
+  login: loginReducer,
+  signup: signupReducer,
+  logout: logoutReducer,
+  news: newsReducer
+});
 
 export const appRootReducer: Reducer<IAppStore> = (
   state: IAppStore | undefined,
-  action: AnyAction,
+  action: AnyAction
 ): IAppStore => {
   if (action.type === RESET_EVERYTHING) {
-    state = undefined
+    state = undefined;
   }
-  return appReducer(state, action)
-}
+  return appReducer(state, action);
+};
